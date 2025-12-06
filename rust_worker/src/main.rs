8be78c83d113d3190706b198cc6baebf875c0a5f
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-12-06 21:23:31 +0100

    Migrating file stuff to rust worker, fetches first file matching id

--- rust_worker/src/main.rs
+++ rust_worker/src/main.rs
@@ -3,25 +3,57 @@ use axum::{
     routing::post,
     Router,
 };
+use sqlx::postgres::PgPoolOptions;
 
 use serde::Deserialize;
+use uuid::Uuid;
 
-#[derive(Deserialize)]
-struct File {
-    file_name: String,
+#[derive(Debug)]
+#[derive(sqlx::FromRow)]
+pub struct DatabaseFile {
+    pub file_id: Uuid,
+    pub filename: String,
+    pub file_type: Option<String>,
+    pub owner_id: Uuid,
+    pub extension: String,
+    pub size: f32,
+    pub url: Option<String>,
+    pub createdat: Option<String>,
+    pub lastmodified: Option<String>,
+    pub shared_with: Vec<String>,
 }
 
-async fn hello(extract::Json(payload): extract::Json<File>) {
-    println!("File name {}", payload.file_name);
+pub async fn get_files(pool: &sqlx::PgPool, file_id: Uuid) -> Result<DatabaseFile, sqlx::Error> {
+    let file = sqlx::query_as::<_, DatabaseFile>("SELECT * FROM files where file_id = $1")
+        .bind(file_id)
+        .fetch_one(pool)
+        .await?;
+    Ok(file)
 }
+
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", post(hello));
+    //postgres set up
+    let database_url = "postgresql://postgres:dinqja123@localhost/servr_db"; 
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url)
+    .await
+    .expect("Failed to create pool");
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Connected to the database!");
+    //
+    let fileID =  Uuid::parse_str("6380a4c5ff79442ca32a6eb506dd3241").unwrap();
+    let file = get_files(&pool, fileID).await.unwrap();
+    println!("File: {:?}", file);
 
-    axum::serve(listener, app)
-        .await
-        .unwrap();
+    //let app = Router::new().route("/hello", get());
+
+    //let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    //axum::serve(listener, app)
+     //   .await
+       // .unwrap();
+
 }
